ef6a98f102d5db34bb888f4b14af8ba508bdbbd6
Aleksandr <aleksanches123@mail.ru>
2022-12-12 11:40:18 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
     }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int[] integer = {1, 2, 3};
+        double[] fraction = {1.57, 7.654, 9.986};
+        int[] arbitrary = {1, 2 , 4, 5, 6, 7};
+
+    }
 }
