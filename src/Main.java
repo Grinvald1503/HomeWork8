586497308225a98628e0283fe65fe5f2e369f9e3
Aleksandr <aleksanches123@mail.ru>
2022-12-12 12:03:43 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -34,6 +35,35 @@ public class Main {
         }
         for (int i = 0; i < arbitrary.length; i++) {
             if (i == arbitrary.length - 1) {
+                System.out.println(arbitrary[i]);
+                break;
+            }
+            System.out.print(arbitrary[i] + ",");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] integer = {1, 2, 3};
+        double[] fraction = {1.57, 7.654, 9.986};
+        int[] arbitrary = {1, 2, 4, 5, 6, 7};
+        for (int i = integer.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(integer[i]);
+                break;
+            }
+            System.out.print(integer[i] + ",");
+
+        }
+        for (int i = fraction.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(fraction[i]);
+                break;
+            }
+            System.out.print(fraction[i] + ",");
+        }
+        for (int i = arbitrary.length - 1; i >= 0; i--) {
+            if (i == 0) {
                 System.out.println(arbitrary[i]);
                 break;
             }
